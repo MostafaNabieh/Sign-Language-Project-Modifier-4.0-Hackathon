5a6b5bb0f2bf16b0e79f382c19cfc49dd972fc79
abdullah-sawey <55079400+abdullah-sawey@users.noreply.github.com>
2020-04-06 10:16:03 +0200

    Add files via upload

---
+++ LCD_config.h
@@ -0,0 +1,12 @@
+/*
+ * LCD_config.h
+ *
+ * Created: 3/23/2020 11:20:55 PM
+ *  Author: abdul
+ */ 
+
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+#define four_bits_mode 
+#endif /* LCD_CONFIG_H_ */
---
+++ LCD_config.h.h
@@ -0,0 +1,16 @@
+/*
+ * LCD_config.h
+ *
+ * Created: 3/23/2020 11:18:41 PM
+ *  Author: abdul
+ */ 
+
+
+#ifndef LCD_CONFIG.H_H_
+#define LCD_CONFIG.H_H_
+
+
+
+
+
+#endif /* LCD_CONFIG.H_H_ */
